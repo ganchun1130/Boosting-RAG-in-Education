14a675de91924d78ada22dd55d2a603e2d5d1151
ganchun1130 <72029219+ganchun1130@users.noreply.github.com>
2024-04-26 15:48:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,32 +7,31 @@ LLM_DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 num_gpus = torch.cuda.device_count()
 
 
-MODEL_CACHE_PATH = "/usr/local/TFBOYS/gc/NLP/ChatXIaoYa_chatglm/cache"
+MODEL_CACHE_PATH = "/cache"
 
 # vector storage config
-VECTOR_STORE_PATH = '/usr/local/TFBOYS/gc/NLP/ChatXIaoYa_chatglm/vector_store'
-VECTOR_STORE_NAME1 = 'XiaoYa_faiss_with_big_chunk_questions_rerank'
-VECTOR_STORE_NAME2 = "XiaoYa_faiss_with_big_chunk_questions_text2vect"
-VECTOR_STORE_NAME3 = 'XiaoYa_faiss_with_small_chunk_questions_rerank'
-VECTOR_STORE_NAME4 = "XiaoYa_faiss_with_small_chunk_questions_text2vect"
+VECTOR_STORE_NAME1 = ''
+VECTOR_STORE_NAME2 = ""
+VECTOR_STORE_NAME3 = ''
+VECTOR_STORE_NAME4 = ""
 
-VECTOR_STORE_NAME = VECTOR_STORE_NAME2
-# other names: XiaoYa_faiss_with_big_chunk_questions_rerank
+VECTOR_STORE_NAME = VECTOR_STORE_NAME1
+
 
 # knowledge file path
-KNOWLEDGE_FILE_PATH = '/usr/local/TFBOYS/gc/NLP/ChatXIaoYa_chatglm/knowledge_file'
-KNOWLEDGE_FILE1 = "promax_data_zh_processed_after_chatglm3.pdf"
-KNOWLEDGE_FILE2  = "promax_data_zh_processed2.pdf"
-KNOWLEDGE_FILE3 = "XiaoYa_Basic_Introduction.pdf"
+KNOWLEDGE_FILE_PATH = KNOWLEDGE_FILE1
+KNOWLEDGE_FILE1 = ""
+KNOWLEDGE_FILE2  = ""
+KNOWLEDGE_FILE3 = ""
 
 # 本地LLM API config
-ChatGLM3_6B_BASE_URL = "http://172.0.101.52:8000/v1/chat/completions"
-Qwen7B_BASE_URL = "http://172.0.101.52:8001/v1/chat/completions"
-Qwen14B_BASE_URL = "http://172.0.101.52:8002/v1/chat/completions"
+ChatGLM3_6B_BASE_URL = "http://localhost:8000/v1/chat/completions"
+Qwen7B_BASE_URL = "http://localhost:8001/v1/chat/completions"
+Qwen14B_BASE_URL = "http://localhost:8002/v1/chat/completions"
 OLLAMA_BASE_URL = "http://localhost:11434/v1"
 
 # ragas评估数据 config
-ragas_dataset_base_path = "/usr/local/TFBOYS/gc/NLP/LLM_RAG_API/Evaluation/data/small_chunk"
+ragas_dataset_base_path = "Evaluation/data"
 ragas_dataset_name = {
     "naive_rag": "naive_eval_data.json",
     "naive_test": "naive_eval_data_test.json",
@@ -66,22 +65,21 @@ text2vector_embedding_model = "text2vec-base"
 
 # model config
 embedding_model_dict = {
-    "bge-reranker-base": "/usr/local/TFBOYS/gc/model/BAAI/bge-reranker-base",
-    "text2vec-base": "/usr/local/TFBOYS/gc/model/text2vec-large-chinese",
-    'simbert-base-chinese': '/usr/local/TFBOYS/gc/model/simbert-base-chinese',
+    "bge-reranker-base": "BAAI/bge-reranker-base",
+    "text2vec-base": "model/text2vec-large-chinese",
+    'simbert-base-chinese': 'model/simbert-base-chinese',
 }
 
 
 llm_model_dict = {
-    "ChatGLM3-6B": "/usr/local/TFBOYS/gc/model/chatglm3-6b-32k",
+    "ChatGLM3-6B": "model/chatglm3-6b-32k",
 
-    "Qwen-7B":"/usr/local/TFBOYS/gc/model/qwen/Qwen-7B-Chat",
-    "Qwen-14B":"/usr/local/TFBOYS/gc/model/qwen/Qwen-14B-Chat",
-    "Qwen1.5-7B":"/usr/local/TFBOYS/gc/model/qwen/Qwen1.5-7B-Chat",
-    "Qwen1.5-14B":"/usr/local/TFBOYS/gc/model/qwen/Qwen1.5-14B-Chat",
+    "Qwen-7B":"qwen/Qwen-7B-Chat",
+    "Qwen-14B":"qwen/Qwen-14B-Chat",
+    "Qwen1.5-7B":"qwen/Qwen1.5-7B-Chat",
+    "Qwen1.5-14B":"qwen/Qwen1.5-14B-Chat",
 
-    "vicuna1.5-13b":"/usr/local/TFBOYS/gc/model/lmsys/vicuna-13b-v1.5-16k",
-
-    "Llama-2-7b":"/usr/local/TFBOYS/gc/model/meta/Llama-2-7b-chat-hf"
+    "vicuna1.5-13b":"lmsys/vicuna-13b-v1.5-16k",
+    "Llama-2-7b":"meta/Llama-2-7b-chat-hf"
 
 }
