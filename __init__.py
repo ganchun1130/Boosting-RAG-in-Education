285f071e0899fde45707179a01a9d9b96d29b75e
ganchun1130 <72029219+ganchun1130@users.noreply.github.com>
2024-04-26 15:49:48 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,3 @@
-# curl http://172.0.101.52:3000/v1/chat/completions \
-#     -H "Content-Type: application/json" \
-#     -H "Authorization: Bearer sk-c4GOPTvMpPQojDDL95F89aE0F8A344F7A2514d943bDe2714" \
-#     -d '{
-#            "model": "ChatGLM3-6B-32K",
-#            "messages": [{"role": "user", "content": "Say this is a test!"}],
-#            "temperature": 0.7
-#          }'
-#
 # curl http://localhost:11434/v1/chat/completions \
 #     -H "Content-Type: application/json" \
 #     -d '{
